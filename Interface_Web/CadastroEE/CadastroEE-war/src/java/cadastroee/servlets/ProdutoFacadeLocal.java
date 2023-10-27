533b30df0233f56002465fb238b414ddfaa64fbc
Ariel Freitas <94645030+ArielFSantos@users.noreply.github.com>
2023-10-27 19:21:19 -0300

    att projeto
    estava em pasta errada kkk

---
+++ Interface_Web/CadastroEE/CadastroEE-war/src/java/cadastroee/servlets/ProdutoFacadeLocal.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cadastroee.servlets;
+
+/**
+ *
+ * @author ariel
+ */
+class ProdutoFacadeLocal {
+
+    MeuServlet.Produto buscarProdutoPorId(int i) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+}
